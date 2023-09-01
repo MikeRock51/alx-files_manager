daedff4b821685250fbf1a8159759bf1b5bee438
MikeRock51 <mikerockmusic51@gmail.com>
2023-09-01 14:31:05 +0100

    Add Task 0

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on("error", (error) => {
+      console.log(error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const asyncGet = promisify(this.client.get).bind(this.client);
+    return asyncGet(key);
+  }
+
+  async set(key, value, duration) {
+    const setExp = promisify(this.client.setex).bind(this.client);
+    return setExp(key, duration, value);
+  }
+
+  async del(key) {
+    const asyncDel = promisify(this.client.del).bind(this.client);
+    return asyncDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
